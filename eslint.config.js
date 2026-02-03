aa8a8585cc3f4357c9521039a68305bccf3e220d
Danilo Arenas Yi <danilo.arenasyi@gmailcom>
2026-02-03 01:19:32 -0500

    chore: update dependencies and add configuration files
    - Updated package.json to include new dependencies for Tailwind CSS, NativeWind, and other libraries.
    - Added postcss.config.mjs for PostCSS configuration with Tailwind CSS.
    - Created global.css to import Tailwind CSS and NativeWind styles.
    - Added tsconfig.json for TypeScript configuration with path aliases and compiler options.

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+// eslint.config.js
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintConfigPrettier = require("eslint-config-prettier");
+
+module.exports = defineConfig([expoConfig, eslintConfigPrettier]);
